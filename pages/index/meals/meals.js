e43c84ccc1b5988a9ac9ef1331cd79cc4617da7f
fengfengying <2119179562@qq.com>
2021-06-11 17:20:05 +0800

    实现左右滑动图片和tab切换样式修改

--- pages/index/meals/meals.js
+++ pages/index/meals/meals.js
@@ -1,22 +1,72 @@
 // pages/index/meals/meals.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    mealsNameList: [{text: '早餐', id: '1'}, {text: '午餐', id: '2'}, {text: '下午茶', id: '3'}, {text: '晚餐', id: '4'}, {text: '夜宵', id: '5'}],
-    className: 'mealsName-text',
+    mealsNameList: [{
+      text: '早餐',
+      id: 0
+    }, {
+      text: '午餐',
+      id: 1
+    }, {
+      text: '下午茶',
+      id: 2
+    }, {
+      text: '晚餐',
+      id: 3
+    }, {
+      text: '夜宵',
+      id: 4
+    }],
+    mealsTabContent: [{
+        img: ['../../../images/meals-img/ym.jpg', '../../../images/meals-img/sjb.jpg', ],
+        id: 0
+      },
+      {
+        img: ['../../../images/meals-img/pg.jpg', '../../../images/meals-img/jc.jpg', ],
+        id: 1
+      },
+      {
+        img: ['../../../images/meals-img/mb.jpg', '../../../images/meals-img/ssb.jpg'],
+        id: 2
+      },
+      {
+        img: ['../../../images/meals-img/tdb.jpg', '../../../images/meals-img/xbg.jpg'],
+        id: 3
+      },
+      {
+        img: ['../../../images/meals-img/yy.jpg', '../../../images/meals-img/jz.jpg', ],
+        id: 4
+      }
+    ],
+    mealsName: 'mealsNametext',
+    mealsActiviy: 'mealsNameactive',
+    chooseId: 0,
+    scrollLeft: 0, //tab标题的滚动条位置
   },
-  
+
   /**
-   * 页面的js方法
+   * 页面的js方法 点击事件
    */
-  clickTab: function(e) {
-    console.log(e.currentTarget.dataset.id)
+  clickTab: function (e) {
+    var cur = e.target.dataset.current;
+    if (this.data.chooseId == cur) {
+      return false;
+    } else {
+      this.setData({
+        chooseId: cur
+      })
+    }
+  },
+  /**
+   * 页面的js方法 滚动切换标签样式
+   */
+  switchTab: function (e) {
     this.setData({
-      className: 'mealsName-active'
-     })
+      chooseId: e.detail.current
+    })
   },
 
 
@@ -31,7 +81,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
